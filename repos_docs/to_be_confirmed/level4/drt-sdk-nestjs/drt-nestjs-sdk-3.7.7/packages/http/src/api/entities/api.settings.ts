4799fc91911e42914269d8d65422d0ba5433bd4f
teja78906 <tejaswaroop0507@gmail.com>
2025-04-21 14:06:41 +0530

    3.7.1 added nestjs

---
+++ repos_docs/to_be_confirmed/level4/drt-sdk-nestjs/drt-nestjs-sdk-3.7.7/packages/http/src/api/entities/api.settings.ts
@@ -0,0 +1,17 @@
+import { NativeAuthSigner } from "../../auth/native.auth.signer";
+
+export interface ApiSettingsBasicCredentials {
+  username: string;
+  password: string;
+}
+
+export class ApiSettings {
+  timeout?: number;
+  skipRedirects?: boolean;
+  responseType?: 'arraybuffer' | 'json';
+  headers?: Record<string, string>;
+  params?: any;
+  httpsAgent?: any;
+  auth?: ApiSettingsBasicCredentials;
+  nativeAuthSigner?: NativeAuthSigner;
+}
