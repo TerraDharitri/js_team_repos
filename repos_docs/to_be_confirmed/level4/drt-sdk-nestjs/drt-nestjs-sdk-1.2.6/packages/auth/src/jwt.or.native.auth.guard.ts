fb6f02e2fee50e94ba8b4f43407483838b857a3f
teja78906 <tejaswaroop0507@gmail.com>
2025-04-21 18:06:20 +0530

    nestjs all version uploaded

---
+++ repos_docs/to_be_confirmed/level4/drt-sdk-nestjs/drt-nestjs-sdk-1.2.6/packages/auth/src/jwt.or.native.auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, CanActivate, ExecutionContext, Inject, Optional } from '@nestjs/common';
+import { CacheService } from '@terradharitri/sdk-nestjs-cache';
+import { DrtnestConfigService, DRTNEST_CONFIG_SERVICE } from '@terradharitri/sdk-nestjs-common';
+import { JwtAuthenticateGuard } from './jwt.authenticate.guard';
+import { NativeAuthGuard } from './native.auth.guard';
+
+@Injectable()
+export class JwtOrNativeAuthGuard implements CanActivate {
+  constructor(
+    @Inject(DRTNEST_CONFIG_SERVICE) private readonly drtnestConfigService: DrtnestConfigService,
+    @Optional() private readonly cacheService?: CacheService,
+  ) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const jwtGuard = new JwtAuthenticateGuard(this.drtnestConfigService);
+    const nativeAuthGuard = new NativeAuthGuard(this.drtnestConfigService, this.cacheService);
+
+    try {
+      const result = await jwtGuard.canActivate(context);
+      if (result) {
+        return true;
+      }
+    } catch (error) {
+      // do nothing
+    }
+
+    try {
+      return await nativeAuthGuard.canActivate(context);
+    } catch (error) {
+      return false;
+    }
+  }
+}
---
+++ repos_docs/to_be_confirmed/level4/drt-sdk-nestjs/drt-nestjs-sdk-1.2.6/packages/common/src/sc.interactions/contract.loader.ts
@@ -0,0 +1,44 @@
+import { SmartContractAbi, AbiRegistry, SmartContract, Address } from "@terradharitri/sdk-core";
+import * as fs from "fs";
+import { OriginLogger } from "../utils/origin.logger";
+
+export class ContractLoader {
+  private readonly logger = new OriginLogger(ContractLoader.name);
+  private readonly abiPath: string;
+  private readonly contractInterface: string;
+  private abi: SmartContractAbi | undefined = undefined;
+
+  constructor(abiPath: string, contractInterface: string) {
+    this.abiPath = abiPath;
+    this.contractInterface = contractInterface;
+  }
+
+  private async load(): Promise<SmartContractAbi> {
+    try {
+      const jsonContent: string = await fs.promises.readFile(this.abiPath, { encoding: "utf8" });
+      const json = JSON.parse(jsonContent);
+
+      const abiRegistry = AbiRegistry.create(json);
+
+      const abi = new SmartContractAbi(abiRegistry, [this.contractInterface]);
+
+      return abi;
+    } catch (error) {
+      this.logger.log(`Unexpected error when trying to create smart contract from abi`);
+      this.logger.error(error);
+
+      throw new Error('Error when creating contract from abi');
+    }
+  }
+
+  async getContract(contractAddress: string): Promise<SmartContract> {
+    if (!this.abi) {
+      this.abi = await this.load();
+    }
+
+    return new SmartContract({
+      address: new Address(contractAddress),
+      abi: this.abi,
+    });
+  }
+}
