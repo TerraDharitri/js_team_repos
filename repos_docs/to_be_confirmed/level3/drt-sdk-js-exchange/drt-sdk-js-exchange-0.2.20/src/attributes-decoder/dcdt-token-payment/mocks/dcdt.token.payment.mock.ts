5dc3e8f5b76a39b78c034af8bb53d90f94c62a9d
teja78906 <tejaswaroop0507@gmail.com>
2025-04-22 12:01:01 +0530

    js-exchange level3 0.2.20

---
+++ repos_docs/to_be_confirmed/level3/drt-sdk-js-exchange/drt-sdk-js-exchange-0.2.20/src/attributes-decoder/dcdt-token-payment/mocks/dcdt.token.payment.mock.ts
@@ -0,0 +1,4 @@
+export const DcdtTokenPaymentAttributesOld =
+    'AAAAAApNT0EtZGMyODljAAAAAAAAAAAAAAAKTV4rYVjgfxuhgA==';
+export const DcdtTokenPaymentAttributesNew =
+    'AAAACk1PQS1kYzI4OWMAAAAAAAAAAAAAAApNXithWOB/G6GA';
