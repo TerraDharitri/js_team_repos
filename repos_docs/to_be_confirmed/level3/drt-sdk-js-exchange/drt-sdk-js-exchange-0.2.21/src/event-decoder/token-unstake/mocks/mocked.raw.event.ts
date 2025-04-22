6d11a0b0fb0d456dea3a265cb9f1b0c2f54c061e
teja78906 <tejaswaroop0507@gmail.com>
2025-04-22 12:10:45 +0530

    0.2.21 level3 js-exchange

---
+++ repos_docs/to_be_confirmed/level3/drt-sdk-js-exchange/drt-sdk-js-exchange-0.2.21/src/event-decoder/token-unstake/mocks/mocked.raw.event.ts
@@ -0,0 +1,14 @@
+import { RawEventType } from '../../generic.types';
+
+export const UnlockedTokensRaw: RawEventType = {
+    identifier: 'depositUserTokens',
+    address: 'drt1qqqqqqqqqqqqqpgq4vukqq9sd040peulr2m0tlnyffgdee770n4syxl30q',
+    data: 'AAAAAAAAC0cAAAANRUxLTU9BLTdlNjg3MwAAAAAAAAAQAAAACQEVjkYJE9AAAAAAAApNT0EtZGMyODljAAAAAAAAAAAAAAAIN8LNtjOFAAAAAAAAAAALRwAAAA1FTEtNT0EtN2U2ODczAAAAAAAAABAAAAAJArXjrxaxiAAAAAAACk1PQS1kYzI4OWMAAAAAAAAAAAAAAAiLZwJHgMyAAAAAAAAAAAtHAAAADUVMS01PQS03ZTY4NzMAAAAAAAAAEAAAAAkFa8deLWMQAAAAAAAKTU9BLWRjMjg5YwAAAAAAAAAAAAAACQEWzgSPAZkAAA==',
+    topics: [
+        'dXNlclVubG9ja2VkVG9rZW5z',
+        'WSAtJp5q/eMo4nNwJZFSGyiznNB1mbKsyH0N8uGIMgU=',
+        'NMYU',
+        'C0Y=',
+        'Y4NRCg==',
+    ],
+};
